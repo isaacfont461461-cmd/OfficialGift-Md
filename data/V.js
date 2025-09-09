4b9bb9975e0c633733e34d9c8b3efd1539f806dc
isaacfont461461-cmd <isaacfont461461@gmail.com>
2025-09-10 00:40:01 +0100

    Update V.js

--- data/V.js
+++ data/V.js
@@ -1,18 +1,4 @@
-
-const axios = require("axios");
-
-async function getLatestVersion() {
-    try {
-        const url = "https://raw.githubusercontent.com/isaacfont461461-cmd/OfficialGift-Md/main/data/V.js";
-        const res = await axios.get(url);
-
-        // Extract version string from module.exports
-        const match = res.data.match(/version:\s*["'`](.*?)["'`]/);
-        return match ? match[1] : null;
-    } catch (err) {
-        console.error("❌ Failed to fetch latest version:", err);
-        return null;
-    }
-}
-
-module.exports = { getLatestVersion };
+// data/V.js
+module.exports = {
+    version: "2.0.0" // <--- 🔥 This is your real version number
+};
