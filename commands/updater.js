ed1130227a1147d40977e726dc1ba8be26c37792
isaacfont461461-cmd <isaacfont461461@gmail.com>
2025-09-10 10:44:47 +0100

    Update updater.js

--- commands/updater.js
+++ commands/updater.js
@@ -212,7 +212,7 @@ setTimeout(() => {
                 if (latestCommitHash === currentHash) {
                     return await context.reply(
                         `✅ Bot is up-to-date!\n\n` +
-                        `Current Version: \`${global.version || 'Unknown'}\`\n` +
+                        `Current Version: \`${latestVersion || 'Unknown'}\`\n` +
                         `Last Commit: ${latestCommitMessage}\n` +
                         `Date: ${commitDate}\n` +
                         `Author: ${author}`
